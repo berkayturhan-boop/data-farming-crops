961fb00e390cb62b864176895f00b7f91c32c075
berkayturhan <r.berkayturhan@gmail.com>
2025-12-28 23:40:45 +0300

    Completed Corn and Rice

--- farm/rice.py
+++ farm/rice.py
@@ -1,5 +1,8 @@
+from farm.crop import Crop
 
-class Rice():
-    # YOUR CODE HERE
-    pass
+class Rice(Crop):
+    def water(self):
+        self.grains += 5
 
+    def transplant(self):
+        self.grains += 10
